b905a25b1c0e0deea9b32582daa11170157362a2
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-06-16 19:33:38 +0530

    Passing  data in ExpenseItem via props and added multiple expenses

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
-      <ExpenseItem> </ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 6)}
+        title="Bike Insurance"
+        amount={2000}
+      />
+      <ExpenseItem
+        date={new Date(2023, 1, 10)}
+        title="Groceries"
+        amount={1700}
+      />
+      <ExpenseItem
+        date={new Date(2023, 1, 16)}
+        title="Cricket Kit"
+        amount={16000}
+      />
+      <ExpenseItem
+        date={new Date(2023, 1, 17)}
+        title="House Rent"
+        amount={12000}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item ">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description ">
-        <h2>Title</h2>
-        <div className="expense-item__price ">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price ">{props.amount}</div>
       </div>
     </div>
   );
