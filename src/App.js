95aeaa1029d92253ae47985ad2aecf10c3605541
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-06-20 16:28:30 +0530

    split the component & created Expense component which holds items also added its respective css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expense";
 
 function App() {
-  const expense = [
+  const items = [
     {
       id: "exp1",
       date: new Date(2023, 1, 6),
@@ -29,26 +29,7 @@ function App() {
   ];
   return (
     <div className="App">
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <Expense expense={items} />
     </div>
   );
 }
