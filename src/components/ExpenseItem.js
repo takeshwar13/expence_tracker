b83146e9d747ee336663fa83807e8a644ce63c76
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-06-20 15:22:22 +0530

    created a component ExpenseDate which is used for handaling date props, also added it's css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
   return (
     <div className="expense-item ">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <ExpenseDate date={props.date} />
+      </div>
       <div className="expense-item__description ">
         <h2>{props.title}</h2>
         <div className="expense-item__price ">{props.amount} ₹</div>
