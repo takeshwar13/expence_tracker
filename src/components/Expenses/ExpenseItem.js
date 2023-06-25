85a51098e25ec87298b8b66344d8e0cd9090ced2
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-06-25 19:52:10 +0530

    updated data with help of state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
+    setTitle("Updated!");
     console.log("Clicked!!");
   };
   return (
@@ -12,7 +15,7 @@ const ExpenseItem = (props) => {
         <ExpenseDate date={props.date} />
       </div>
       <div className="expense-item__description ">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price ">{props.amount} ₹</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
