b9b8f38db7a111bf0341cb6b41d8b3d4d2084282
Takeshwar Verma <takeshwarverma132@gmail.com>
2023-07-01 19:47:55 +0530

    Dynamically outputing data with the help of using stateful lists and Map function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
+import React, { useState } from "react";
 import Expense from "./components/Expenses/Expense";
 import NewExpense from "./components/AddExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "exp1",
+    date: new Date(2023, 1, 6),
+    title: "Bike Insurance",
+    amount: 2000,
+  },
+  {
+    id: "exp2",
+    date: new Date(2020, 1, 10),
+    title: "Groceries",
+    amount: 1700,
+  },
+  {
+    id: "exp3",
+    date: new Date(2021, 1, 16),
+    title: "Cricket Kit",
+    amount: 16000,
+  },
+  {
+    id: "exp4",
+    date: new Date(2019, 1, 17),
+    title: "House Rent",
+    amount: 12000,
+  },
+];
+
 function App() {
-  const items = [
-    {
-      id: "exp1",
-      date: new Date(2023, 1, 6),
-      title: "Bike Insurance",
-      amount: 2000,
-    },
-    {
-      id: "exp2",
-      date: new Date(2020, 1, 10),
-      title: "Groceries",
-      amount: 1700,
-    },
-    {
-      id: "exp3",
-      date: new Date(2021, 1, 16),
-      title: "Cricket Kit",
-      amount: 16000,
-    },
-    {
-      id: "exp4",
-      date: new Date(2019, 1, 17),
-      title: "House Rent",
-      amount: 12000,
-    },
-  ];
+  const [items, setItems] = useState(DUMMY_EXPENSES);
 
   const expenseHandler = (addExpense) => {
-    console.log(addExpense);
+    setItems((prevExpenses) => {
+      return [addExpense, ...prevExpenses];
+    });
   };
   return (
     <div className="App">
